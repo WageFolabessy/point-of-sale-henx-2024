3fe2c65d76920632aaa81e9060440a6659000949
Wage Folabessy <mbappepsg336@gmail.com>
2024-10-24 01:32:10 +0700

    memperbaiki project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Produk;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Produk::factory(9)->create();
+
+        User::create([
+            'nama' => 'Admin',
+            'username' => 'admin',
+            'password' => 'password',
+            'is_admin' => true,
+        ]);
+    }
+}
